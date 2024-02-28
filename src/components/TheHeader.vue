4e69449c1dabc125b6a46f857344cd1e0a38dfc3
magalutfullaev <mhoja9494@gmail.com>
2024-02-28 13:21:54 +0500

    add: Header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <RouterLink to="/">Tododo</RouterLink>
+    <nav>
+      <RouterLink to="/">Main</RouterLink>
+      <RouterLink to="/todo">Todos</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  padding: 30px 50px;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+    color: #444;
+    font-size: 20px;
+    transition: .3s;
+    &:hover, &.router-link-active  {
+      color: #3A82EE
+    }
+  }
+  nav {
+    display: flex;
+    gap: 20px;
+  }
+}
+</style>
