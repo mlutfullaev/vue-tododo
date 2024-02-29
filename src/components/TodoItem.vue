1120f435a9cad774625becdcb45edff5ef459deb
magalutfullaev <mhoja9494@gmail.com>
2024-02-29 16:56:39 +0500

    add: Edit task

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,14 +1,24 @@
 <script lang='ts' setup>
 import { TodoT } from '@/assets/types'
-defineProps<{ todo: TodoT, i: number }>()
-defineEmits<{(event: 'delete', createdAt: number): void, (event: 'update', i: number, todo: TodoT): void }>()
+import { ref, watch } from 'vue'
+const props = defineProps<{ todo: TodoT, i: number }>()
+const emit = defineEmits<{(event: 'delete', createdAt: number): void, (event: 'update', i: number, todo: TodoT): void }>()
 
+const name = ref(props.todo.name)
+const description = ref(props.todo.description)
+
+watch(name, () => {
+  emit('update', props.i, { ...props.todo, name: name.value })
+})
+watch(description, () => {
+  emit('update', props.i, { ...props.todo, description: description.value })
+})
 </script>
 
 <template>
  <div :class="`todo-item ${todo.type}${todo.checked ? ' done' : ''}`">
-   <h2>{{todo.name}}</h2>
-   <p v-if="todo.description">{{todo.description}}</p>
+   <input type="text" class="name" v-model="name">
+   <input v-if="todo.description" type="text" class="description" v-model="description">
    <div class="actions">
      <button class="delete" @click="$emit('delete', todo.createdAt)">Delete</button>
      <button class="done" @click="$emit('update', i, {...todo, checked: !todo.checked})">{{todo.checked ? 'Undone' : 'Done'}}</button>
@@ -37,8 +47,24 @@ defineEmits<{(event: 'delete', createdAt: number): void, (event: 'update', i: nu
     font-size: 24px;
     padding-bottom: 10px;
   }
-  p {
-    padding-bottom: 10px;
+  input {
+    width: 100%;
+    padding: 6px 10px;
+    border: none;
+    margin-bottom: 10px;
+    outline: none;
+    border-bottom: 1px solid transparent;
+    transition: .3s;
+
+    &:focus {
+      border-bottom-color: #3A82EE;
+    }
+    &.name {
+      font-size: 20px;
+    }
+    &.description {
+      font-size: 16px;
+    }
   }
   .actions {
     padding-top: 10px;
